00bca867700e6c738c760165ac39346c40f03ed2
VoltChange <83451689+VoltChange@users.noreply.github.com>
2021-06-18 23:39:46 +0800

    Add files via upload

---
+++ Resources/blocks.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.6.0" name="blocks" tilewidth="16" tileheight="16" tilecount="768" columns="32">
+ <image source="blocks.png" width="512" height="384"/>
+</tileset>
---
+++ Resources/special.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.6.0" name="special" tilewidth="16" tileheight="16" tilecount="10160" columns="127">
+ <image source="special.png" width="2032" height="1280"/>
+</tileset>
